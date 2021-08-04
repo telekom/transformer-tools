3deaca35ef39949fdc3228fa6c851979017a8bb5
PhilipMay <philip@may.la>
2021-08-04 11:23:20 +0200

    move __version__ to __init__.py

--- transformer_tools/__init__.py
+++ transformer_tools/__init__.py
@@ -5,7 +5,10 @@
 """Transformer-Tools main package."""
 
 from transformer_tools.transformers import LabeledDataset
-from transformer_tools.version import __version__
 
+
+# Versioning follows the `Semantic Versioning Specification <https://semver.org/>`__ and
+# `PEP 440 -- Version Identification and Dependency Specification <https://www.python.org/dev/peps/pep-0440/>`__.  # noqa: E501
+__version__ = "0.0.1rc8"
 
 __all__ = ["LabeledDataset", "__version__"]
