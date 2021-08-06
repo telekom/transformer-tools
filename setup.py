d295dc82f000ac0618d18d3fb8ac8e804f07665a
PhilipMay <philip@may.la>
2021-08-06 12:42:02 +0200

    add missing optional dependencies

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ extras_require = {
         "pylint",
         "pylintfileheader",
     ],
-    "optional": ["ml-cloud-tools", "mlflow", "seldon_core"],
+    "optional": ["ml-cloud-tools", "mlflow", "seldon_core", "cloudpickle", "pyyaml"],
     "testing": ["pytest"],
     # "doc": ["sphinx", "sphinx_rtd_theme", "myst_parser", "sphinx_copybutton"],
 }
