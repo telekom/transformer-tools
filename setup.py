e975cff6dc23139af821bd1754628944045b0dd8
PhilipMay <philip@may.la>
2021-08-08 18:31:59 +0200

    add test for __version__

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ extras_require = {
         "pylintfileheader",
     ],
     "optional": ["ml-cloud-tools", "mlflow", "seldon_core", "cloudpickle", "pyyaml"],
-    "testing": ["pytest"],
+    "testing": ["pytest", "packaging"],
     # "doc": ["sphinx", "sphinx_rtd_theme", "myst_parser", "sphinx_copybutton"],
 }
 
